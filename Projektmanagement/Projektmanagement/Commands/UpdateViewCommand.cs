9a7660205c774ddecd1cd1bcdd879d549de0528b
sirtheta <neo38@bluewin.ch>
2022-02-28 21:57:52 +0100

    intitial commit

---
+++ Projektmanagement/Projektmanagement/Commands/UpdateViewCommand.cs
@@ -0,0 +1,37 @@
+﻿using Projektmanagement.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Projektmanagement.Commands
+{
+  internal class UpdateViewCommand : ICommand
+  {
+
+    private MainViewModel viewModel;
+
+    public UpdateViewCommand(MainViewModel viewModel)
+    {
+      this.viewModel = viewModel;
+    }
+
+    public event EventHandler CanExecuteChanged;
+
+    public bool CanExecute(object parameter)
+    {
+      return true;
+    }
+
+    public void Execute(object parameter)
+    {
+      if (parameter.ToString() == "Home")
+      {
+        viewModel.SelectedViewModel = new HomeViewModel();
+      }
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Introduction 
+Fallstudie Projektmanagementtool im Fach Software-Engineering
+
+# Einfachmal Started
+TBD: Mit was entwickeln wir die app? WPF? Winform? .net core oder .net Framework?
+# Build and Test
+
+
+# Contribute
