ed76282efd2f16d17762c88c94f0e0841b295de5
Marcel Rupp <marcel.rupp@edu.teko.ch>
2022-03-14 22:07:50 +0100

    Phasen Liste in Projekt erstellt

--- Projektmanagement/Projektmanagement/MainClasses/Phase.cs
+++ Projektmanagement/Projektmanagement/MainClasses/Phase.cs
@@ -2,5 +2,11 @@
 {
   internal class Phase
   {
+    public Phase(string phasenname)
+    {
+      Phasenname = phasenname;
+    }
+
+    public string Phasenname { get; set; }
   }
 }
--- Projektmanagement/Projektmanagement/MainClasses/Project.cs
+++ Projektmanagement/Projektmanagement/MainClasses/Project.cs
@@ -1,15 +1,41 @@
-﻿namespace Projektmanagement.MainClasses
+﻿using System.Collections.Generic;
+namespace Projektmanagement.MainClasses
 {
   internal class Project
   {
-    //I ha das mau zum teschte gmacht... bitte gliich no vervouständige aber bitte dr name vom "ProjectName"-getter biiphaute :)
-    private string projectName;
+    private string _projectName;
+    private ProcessModel _pm;
+
 
     public Project(string projectName)
     {
-      this.projectName = projectName;
+      _projectName = projectName;
+    }
+    public Project(string projectName, ProcessModel pm)
+    {
+      _projectName = projectName;
+      _pm = pm;
+
     }
 
-    public string ProjectName => projectName;
+    public List<Phase> MyPhasen()
+    {
+      if (_pm.PhaseNames.Count == 0) {
+        return new List<Phase>();
+
+      }
+
+      List<Phase> phases = new List<Phase>();
+
+      for (int i = 0; i < _pm.PhaseNames.Count; i++) {
+        phases.Add(new Phase(_pm.PhaseNames[i]));
+
+
+      }
+      return phases;
+    }
+
+
+    public string ProjectName => _projectName;
   }
 }
